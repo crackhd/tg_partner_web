192433e2556a4042f516d402c3f8b8b03864a85d
nobody <nobody@localhost>
2020-06-18 11:31:14 -0700

    Updates

---
+++ precache-manifest.7290541c0435f50b553b2f10d5161506.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103cdf42e843caf1f531833fcc49add5",
+    "url": "/tg_partner_web/index.html"
+  },
+  {
+    "revision": "ba72657fd2d4a2d123b4",
+    "url": "/tg_partner_web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1f4d7759c0d323ec8fa4",
+    "url": "/tg_partner_web/static/js/2.167444d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tg_partner_web/static/js/2.167444d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba72657fd2d4a2d123b4",
+    "url": "/tg_partner_web/static/js/main.fc9914d6.chunk.js"
+  },
+  {
+    "revision": "9d276612ea984e8c4863",
+    "url": "/tg_partner_web/static/js/runtime-main.4d0bbd3f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tg_partner_web/static/media/logo.5d5d9eef.svg"
+  }
+]);
